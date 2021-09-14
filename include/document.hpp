1fb6929de04b8377ada118e603c737dafa03762f
Fiorentino, Stefano <stefano.fiorentino@adesso.ch>
2021-09-14 15:31:11 +0200

    change to shared_ptr to const concept getting rid of copies

--- include/document.hpp
+++ include/document.hpp
@@ -12,14 +12,7 @@ void draw(const T& x, std::ostream& out, size_t position)
 class object_t {
 public: 
     template <typename T>
-    object_t(T x) : self_(std::make_unique<model_t<T>>(std::move(x))) {}
-
-    object_t(const object_t& x) : self_(x.self_->copy_()) {}
-    object_t(object_t&&) noexcept = default;
-
-    object_t& operator=(const object_t& x) noexcept
-    { return *this = object_t(x); }
-    object_t& operator=(object_t&&) noexcept = default;
+    object_t(T x) : self_(std::make_shared<model_t<T>>(std::move(x))) {}
     
     friend void draw(const object_t&x, std::ostream& out, size_t position)
     { x.self_->draw(out, position); }
@@ -27,22 +20,19 @@ private:
     struct concept_t
     {
         virtual ~concept_t() = default;
-        virtual std::unique_ptr<concept_t> copy_() const = 0;
         virtual void draw(std::ostream& out, size_t position) const = 0;
     };
     template <typename T>
     struct model_t final : concept_t
     {
         model_t(T x) : data_(std::move(x)){}
-        std::unique_ptr<concept_t> copy_() const override 
-        { return std::make_unique<model_t<T>>(*this); }
         void draw(std::ostream& out, size_t position) const override
         {
             ::draw(data_, out, position);
         }
         T data_;
     };
-    std::unique_ptr<concept_t> self_;
+    std::shared_ptr<const concept_t> self_;
 };
 
 using document_t = std::vector<object_t>;
